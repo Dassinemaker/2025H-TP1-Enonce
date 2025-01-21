fc1105b9be002ed146c7c99e6bb645ce54bdbd68
Dassinemaker <dassine.amiar@polymtl.ca>
2025-01-21 17:20:55 -0500

    exo1

--- exo1.py
+++ exo1.py
@@ -1,8 +1,13 @@
 # Demander le nom complet de l'utilisateur
 # Demander l'âge de l'utilisateur
-name = input("veuillez saisir votre nom :")
+
 # Définir l'année actuelle
 # Calculer l'année de naissance
 
 # Afficher un message de bienvenue avec le nom complet
 # Afficher l'année de naissance
+name = input("Veuillez entrer votre nom complet : :")
+age = int(input("Veuillez entrer votre âge : : "))
+anne = (2025 - age)
+print( "Bonjou" ,name)
+print("Vous êtes né(e) en", anne)
